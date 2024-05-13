061930e3881730e7c60a547c21ef475f3f230f4c
Kizeee <m4ulanarizky261@gmail.com>
2024-05-13 16:42:39 +0700

    rizky

--- src/App.vue
+++ src/App.vue
@@ -6,29 +6,53 @@
       <button @click="addActivity">Tambah</button>
     </div>
 
-    <ul>
+    <ul v-if="!showingPosts">
       <li v-for="(activity, index) in filteredActivities" :key="index" class="activity-item">
-        <span :class="{ completed: activity.completed }">{{ activity.name }}</span>
+        <span v-if="index !== editingIndex" :class="{ completed: activity.completed }">{{ activity.name }}</span>
+        <input v-else type="text" v-model="editedActivityName" @keyup.enter="saveEdit" @keyup.esc="cancelEdit">
         <div>
           <button @click="toggleCompletion(index)" class="action-button">{{ activity.completed ? 'Batal Checklist' : 'Checklist' }}</button>
+          <button @click="editActivity(index)" class="action-button">{{ index === editingIndex ? 'Simpan' : 'Edit' }}</button>
           <button @click="removeActivity(index)" class="action-button">Hapus</button>
         </div>
       </li>
     </ul>
 
-    <div class="filter-co ntainer">
-    <input type="checkbox" id="showOnlyPending" v-model="showOnlyPending">
-    <label for="showOnlyPending">Tampilkan hanya kegiatan yang belum selesai</label>
+    <div class="filter-container" v-if="!showingPosts">
+      <input type="checkbox" id="showOnlyPending" v-model="showOnlyPending">
+      <label for="showOnlyPending">Tampilkan hanya kegiatan yang belum selesai</label>
     </div>
+
+    <button @click="showPostsView" v-if="!showingPosts">Tampilkan Postingan</button>
+
+    <div v-if="showingPosts">
+      <h1>Postingan</h1>
+      <select v-model="selectedUser">
+        <option value="">Select user</option>
+        <option v-for="user in users" :key="user.id" :value="user.id">{{ user.name }}</option>
+      </select>
+      <ul v-if="selectedUser">
+        <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+      </ul>
+    </div>
+    
+    <button @click="showTodosView" v-if="showingPosts">Tampilkan Daftar Kegiatan</button>
   </div>
 </template>
+
 <script>
 export default {
   data() {
     return {
       newActivity: '',
+      editedActivityName: '',
+      editingIndex: -1,
       activities: [],
-      showOnlyPending: false
+      showOnlyPending: false,
+      showingPosts: false,
+      selectedUser: null,
+      users: [],
+      posts: []
     };
   },
   methods: {
@@ -43,6 +67,51 @@ export default {
     },
     toggleCompletion(index) {
       this.activities[index].completed = !this.activities[index].completed;
+    },
+    editActivity(index) {
+      this.editedActivityName = this.activities[index].name;
+      this.editingIndex = index;
+    },
+    saveEdit() {
+      if (this.editedActivityName.trim() !== '') {
+        this.activities[this.editingIndex].name = this.editedActivityName;
+        this.cancelEdit();
+      }
+    },
+    cancelEdit() {
+      this.editedActivityName = '';
+      this.editingIndex = -1;
+    },
+    async fetchUsers() {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json();
+        this.users = data;
+      } catch (error) {
+        console.error('Error Fetching Users:', error);
+      }
+    },
+    async fetchPosts() {
+      if (!this.selectedUser) return;
+      try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${this.selectedUser}`);
+        const data = await response.json();
+        this.posts = data;
+      } catch (error) {
+        console.error('Error Fetching Posts:', error);
+      }
+    },
+    showPostsView() {
+      this.showingPosts = true;
+      this.fetchUsers();
+    },
+    showTodosView() {
+      this.showingPosts = false;
+    }
+  },
+  watch: {
+    selectedUser() {
+      this.fetchPosts();
     }
   },
   computed: {
@@ -66,7 +135,7 @@ export default {
 
 h1 {
   text-align: center;
-  color: #333;
+  color: #cccddb;
 }
 
 .input-container {
